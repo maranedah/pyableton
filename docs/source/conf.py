41cf894b198fed40601c6474091be862691e99d9
maranedah <mauricio.araneda@ug.uchile.cl>
2024-01-09 10:53:04 -0300

    conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,8 @@
 import os
 import sys
 
+import sphinx_rtd_theme  # noqa
+
 sys.path.insert(0, os.path.abspath((os.path.join("..", ".."))))
 
 # -- Project information -----------------------------------------------------
